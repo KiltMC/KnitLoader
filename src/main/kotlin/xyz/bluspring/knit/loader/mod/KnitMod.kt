982b44fefe417a83e3de824ecf2f6580c92c0b92
Naz <bluspringindohq@gmail.com>
2025-08-29 19:41:52 +0800

    Add custom toString to Knit mods I realized how difficult it was to debug without it

--- src/main/kotlin/xyz/bluspring/knit/loader/mod/KnitMod.kt
+++ src/main/kotlin/xyz/bluspring/knit/loader/mod/KnitMod.kt
@@ -4,5 +4,7 @@ package xyz.bluspring.knit.loader.mod
  * Stores information about the mod that Knit can store and other loaders to use.
  */
 abstract class KnitMod(val definition: ModDefinition) {
-
+    override fun toString(): String {
+        return "${this.javaClass.simpleName}[id=${definition.id},name=${definition.displayName}]"
+    }
 }

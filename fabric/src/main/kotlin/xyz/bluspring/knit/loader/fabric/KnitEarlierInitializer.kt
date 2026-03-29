d71d0a520908e1b628ed5604709098fa55c4fb32
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2026-03-29 14:24:53 +0200

    Revert "Use kilt to find the mods directory if possible."
    This reverts commit 23b11f9e4787c83d03c98d2a5579861ab7bdaeb1.

--- fabric/src/main/kotlin/xyz/bluspring/knit/loader/fabric/KnitEarlierInitializer.kt
+++ fabric/src/main/kotlin/xyz/bluspring/knit/loader/fabric/KnitEarlierInitializer.kt
@@ -4,36 +4,16 @@ import de.florianreuth.asmfabricloader.api.event.PrePrePreLaunchEntrypoint
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import net.fabricmc.loader.api.FabricLoader
-import net.fabricmc.loader.api.metadata.ModOrigin
 import xyz.bluspring.knit.loader.KnitLoader
-import java.nio.file.Path
 
 class KnitEarlierInitializer : PrePrePreLaunchEntrypoint {
-    fun getDirectoryContainingTheModsDirectory(): Path {
-        if (FabricLoader.getInstance().isDevelopmentEnvironment) return FabricLoader.getInstance().gameDir
-        // Use the mod container to find the mods directory if possible, to account for mods like AutoModpack which use a different directory.
-        var container = FabricLoader.getInstance().getModContainer("kilt")
-        // Just in case someone decides to bundle Kilt for some reason.
-        while (container.isPresent && container.get().containingMod.isPresent) {
-            container = container.get().containingMod
-        }
-        return if (
-            container.isPresent && container.get().origin.kind != ModOrigin.Kind.PATH ||
-            container.get().origin.paths.isEmpty()
-        ) {
-            FabricLoader.getInstance().gameDir
-        } else {
-            container.get().origin.paths[0].parent.parent
-        }
-    }
-
     override fun onLanguageAdapterLaunch() {
         val loader = KnitLoaderFabric()
 
         runBlocking(Dispatchers.IO) {
             // Scans the mods from the game directory.
             try {
-                loader.scanMods(getDirectoryContainingTheModsDirectory())
+                loader.scanMods(FabricLoader.getInstance().gameDir)
             } catch (e: Throwable) {
                 KnitLoader.instance.displayError("Errors occurred during mod scanning!", RuntimeException(e))
             }

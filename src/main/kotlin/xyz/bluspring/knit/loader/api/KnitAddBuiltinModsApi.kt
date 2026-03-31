443fce701015474f9a3aae65defcda98360fe436
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2026-03-31 11:36:33 +0200

    Added some documentation.

--- src/main/kotlin/xyz/bluspring/knit/loader/api/KnitAddBuiltinModsApi.kt
+++ src/main/kotlin/xyz/bluspring/knit/loader/api/KnitAddBuiltinModsApi.kt
@@ -2,6 +2,12 @@ package xyz.bluspring.knit.loader.api
 
 import xyz.bluspring.knit.loader.mod.ModDefinition
 
+/**
+ * Simple API interface with the ability to add built-in mod definitions.
+ */
 interface KnitAddBuiltinModsApi: KnitApi {
+    /**
+     * Adds the given mod definition alongside the built-in mod definitions for the current loader.
+     */
     fun addModDefinition(mod: ModDefinition)
 }
--- src/main/kotlin/xyz/bluspring/knit/loader/api/KnitApi.kt
+++ src/main/kotlin/xyz/bluspring/knit/loader/api/KnitApi.kt
@@ -2,6 +2,12 @@ package xyz.bluspring.knit.loader.api
 
 import xyz.bluspring.knit.loader.KnitModLoader
 
+/**
+ * Simple API interface with access to the loader.
+ */
 interface KnitApi {
+    /**
+     * Access to the loader
+     */
     val loader: KnitModLoader<*>
 }
